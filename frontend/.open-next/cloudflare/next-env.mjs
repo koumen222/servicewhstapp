c9d85f2f26cfea657c3ec3b3190a21f285f33106
koumen222 <koumenprive@gmail.com>
2026-03-07 09:09:12 +0100

    wer

---
+++ frontend/.open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"NEXT_PUBLIC_API_URL":"https://api.ecomcookpit.site","NEXT_PUBLIC_APP_NAME":"WhatsApp SaaS","NEXT_PUBLIC_APP_URL":"https://servicewhstapps.pages.dev"};
+export const development = {"NEXT_PUBLIC_API_URL":"https://api.ecomcookpit.site","NEXT_PUBLIC_APP_NAME":"WhatsApp SaaS","NEXT_PUBLIC_APP_URL":"https://servicewhstapps.pages.dev"};
+export const test = {"NEXT_PUBLIC_API_URL":"https://api.ecomcookpit.site","NEXT_PUBLIC_APP_NAME":"WhatsApp SaaS","NEXT_PUBLIC_APP_URL":"https://servicewhstapps.pages.dev"};
